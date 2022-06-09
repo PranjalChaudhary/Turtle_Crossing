610ec45db650ee4cf943492725741e50bfe515f9
PranjalChaudhary <pranjal15lp@gmail.com>
2022-06-09 21:28:46 +0530

    Final commit

--- player.py
+++ player.py
@@ -11,13 +11,14 @@ class Player(Turtle):
         super().__init__()
         self.shape("turtle")
         self.penup()
-        self.goto(STARTING_POSITION)
+        self.reset_player()
         self.setheading(90)
 
     def move_up(self):
         self.forward(MOVE_DISTANCE)
 
     def reset_player(self):
+        """Moves the player to the starting position after completion of level"""
         self.goto(STARTING_POSITION)
 
 
